8b4c7e4f180140dd96ef3bfe2b55ff4cd9c02927
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-09-15 16:14:53 +0530

    toggle in add transport

--- src/Reusable_components/ToggleButton.jsx
+++ src/Reusable_components/ToggleButton.jsx
@@ -1,49 +1,31 @@
 // ToggleButton.js
 import React from 'react';
+import './Switch.css';
 
-const ToggleButton = ({ id, label, defaultChecked = true, register, ...rest }) => {
+const ToggleButton = ({ id, label, isOn, handleToggle, register, ...rest }) => {
   return (
     <div className="flex items-center">
       <label htmlFor={id} className="mr-3 text-sm font-medium text-gray-700">
         {label}
       </label>
-      <input
-        id={id}
-        type="checkbox"
-        {...register(id)} // Use the register method from react-hook-form
-        defaultChecked={defaultChecked} // Default value
-        {...rest}
-        className="toggle-checkbox"
-      />
-      <style jsx>{`
-        .toggle-checkbox {
-          appearance: none;
-          background-color: #e5e7eb; /* Gray background */
-          cursor: pointer;
-          border-radius: 9999px;
-          position: relative;
-          width: 2.5rem;
-          height: 1.5rem;
-          transition: background-color 0.2s ease-in-out;
-        }
-        .toggle-checkbox:checked {
-          background-color: #4ade80; /* Green when active */
-        }
-        .toggle-checkbox::before {
-          content: '';
-          position: absolute;
-          top: 0.25rem;
-          left: 0.25rem;
-          width: 1.25rem;
-          height: 1.25rem;
-          background-color: white;
-          border-radius: 9999px;
-          transition: transform 0.2s ease-in-out;
-        }
-        .toggle-checkbox:checked::before {
-          transform: translateX(1rem); /* Move knob to the right */
-        }
-      `}</style>
+      <div className="relative">
+        <input
+          id={id} // Use the id prop here
+          defaultChecked={isOn}
+          {...register(id)} // Use the register method from react-hook-form
+          onChange={handleToggle}
+          className="react-switch-checkbox"
+          type="checkbox"
+          {...rest}
+        />
+        <label
+          style={{ background: isOn ? '#06D6A0' : 'grey' }}
+          className="react-switch-label"
+          htmlFor={id} // Use the id prop for label association
+        >
+          <span className={`react-switch-button`} />
+        </label>
+      </div>
     </div>
   );
 };
