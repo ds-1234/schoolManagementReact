abf1b30f9b3e74f6830b98c1743a57e40e23c518
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-09-24 17:54:10 +0530

    income

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,6 @@ import Invoices from './Dashboard/Admin_Dashboard/Accounts/Invoices/Invoices';
 import InvoiceView from './Dashboard/Admin_Dashboard/Accounts/Invoice View/InvoiceView';
 import Transactions from './Dashboard/Admin_Dashboard/Accounts/Transactions/Transactions';
 import AddExpenses from './Dashboard/Admin_Dashboard/Accounts/Expenses/AddExpenses';
-import AddIncome from './Dashboard/Admin_Dashboard/Accounts/Income/AddIncome';
 
 function App() {
   return (
