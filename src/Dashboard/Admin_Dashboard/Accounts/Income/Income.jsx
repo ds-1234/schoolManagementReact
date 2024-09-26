4be2a57eefee355ed03732d35c7a839b6ea6dc1b
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-09-26 16:20:30 +0530

    income uii

--- src/Dashboard/Admin_Dashboard/Accounts/Income/Income.jsx
+++ src/Dashboard/Admin_Dashboard/Accounts/Income/Income.jsx
@@ -186,7 +186,7 @@ function Income() {
   ];
 
   return (
-    <div className=' h-full mb-10 mr-10'>
+    <div className=' h-full mb-10 -mr-10'>
       <h1 className='text-lg md:text-2xl pt-8 font-semibold text-black'>Income</h1>
       <p className='mt-2'>
         Dashboard /<NavLink to='/admin'> Admin </NavLink>/
