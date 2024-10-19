31954940c79326515ca0ca743162544ccbf3bd1e
YukiP7 <gargyukti112@gmail.com>
2024-10-19 11:23:17 +0530

    changes in user filter

--- src/Dashboard/Admin_Dashboard/User/User.jsx
+++ src/Dashboard/Admin_Dashboard/User/User.jsx
@@ -93,13 +93,21 @@ const column = [
   }, []);
 
   const searchOptions = [
-    // {label : 'Role' , value: 'role'} ,
+    {label : 'Role' , value: 'role'} ,
     { label: 'First Name', value: 'firstName' },
     { label: 'Last Name', value: 'lastName' },
     { label: 'Email', value: 'email' },
     { label: 'Phone', value: 'phone' },
     { label: 'Gender', value: 'gender' },
   ];
+
+  const roleMapping = {
+    1: 'Guest' ,
+    2: 'Admin',
+    3: 'Student',
+    4: 'Teacher',
+    5: 'Parents'
+  }
 
   // Handle Search Logic
   const handleSearch = (query, checkboxRefs) => {
@@ -113,11 +121,12 @@ const column = [
   
     const filteredData = filterUser.filter((row) =>
       selectedFields.some((field) => {
-        // if (field === 'role') {
-        //   return row.role?.toLowerCase().includes(query.toLowerCase());
-        // }else{
+        if (field === 'role') {
+          const roleName = roleMapping[row.role];
+          return roleName?.toLowerCase().includes(query.toLowerCase());
+        }else{
         return row[field]?.toLowerCase().includes(query.toLowerCase())
-        // }
+        }
       }
       )
     );
