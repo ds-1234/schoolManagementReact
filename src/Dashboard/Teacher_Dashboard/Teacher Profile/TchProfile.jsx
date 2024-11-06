42b527c0506688c37d53206338d37ca329e3dd68
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-11-06 18:54:18 +0530

    teacher profile

--- src/Dashboard/Teacher_Dashboard/Teacher Profile/TchProfile.jsx
+++ src/Dashboard/Teacher_Dashboard/Teacher Profile/TchProfile.jsx
@@ -10,39 +10,98 @@ import femaleImg from '../../../assets/woman.png'
 
 const TchProfile = () => {
   const user = JSON.parse(sessionStorage.getItem('user'));
-  const [studentDetails, setStudentDetails] = useState(null);
+  const [teacherDetails, setTeacherDetails] = useState(null);
+  const [teacherBasicDetails, setTeacherBasicDetails] = useState(null);
   const [activeTab, setActiveTab] = useState('hostel'); 
   const [hostel , setHostel] = useState({}) 
   const [transport , setTransport] = useState({})
   const [hostelRoom , setHostelRoom] = useState({}) 
+  const [classList, setClassList] = useState([]);
+  const [filteredClasses, setFilteredClasses] = useState([]);
 
-  useEffect(() => {
+
+console.log(filteredClasses,'classes')
+
+
+    useEffect(() => {
+      const fetchBasicInfo = () => {
+        axios({
+          method: 'GET',
+          url: `${BASE_URL}/teacherInfo/getTeacherInfo/${user.id}`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => {
+            console.log('Data from API setTeacherBasicDetails:', response.data);
+            setTeacherBasicDetails(response.data.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          });
+      };
+  
+      fetchBasicInfo();
+    },[user.id,teacherDetails]);
+
+
+    useEffect(() => {
+
     const fetchData = () => {
       axios({
         method: 'GET',
-        url: `${BASE_URL}/user/getStudentDetails/${user.userId}`,
+        url: `${BASE_URL}/user/getUser/${user.id}`,
         headers: {
           'Content-Type': 'application/json',
         },
       })
         .then((response) => {
-          console.log('Data from API:', response.data);
-          setStudentDetails(response.data.data);
-        })
+          console.log('Data from API teacherDetails:', response.data);
+          if (response.data && response.data.data) {
+            setTeacherDetails(response.data.data); // Ensure 'data' exists before setting state
+          }
+                })
         .catch((error) => {
           console.error('Error fetching data:', error);
         });
     };
 
     fetchData();
-  }, [user.userId]);
+  }, [user.id]);
+
+
+  // Fetch Class List
+  const fetchClassList = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/class/getClassList');
+      const data = await response.json();
+      setClassList(data.data);  // Assuming data.data holds the list of classes
+    } catch (error) {
+      console.error('Error fetching class list:', error);
+    }
+  };
+
+  // Filter classes based on the teacher's className array
+  useEffect(() => {
+    if (teacherDetails && classList.length > 0) {
+      const teacherClassIds = teacherDetails.className;  // Array of class IDs from teacher details
+      const filtered = classList.filter(classItem =>
+        teacherClassIds.includes(classItem.id)
+      );
+      setFilteredClasses(filtered);
+    }
+  }, [teacherDetails, classList]);
 
   useEffect(() => {
-    if( studentDetails ){
+    fetchClassList();
+  }, []);
+
+  useEffect(() => {
+    if( teacherDetails ){
         const fetchHostel = () => {
         axios({
           method: 'GET',
-          url: `${BASE_URL}/hostel/getHostelById/${studentDetails.buildingName}`,
+          url: `${BASE_URL}/hostel/getHostelById/${teacherDetails.buildingName}`,
           headers: {
             'Content-Type': 'application/json',
           },
@@ -59,7 +118,7 @@ const TchProfile = () => {
       const fetchHostelRooms = () => {
         axios({
           method: 'GET',
-          url: `${BASE_URL}/hostelRooms/getHostelRoomsById/${studentDetails.roomNumber}`,
+          url: `${BASE_URL}/hostelRooms/getHostelRoomsById/${teacherDetails.roomNumber}`,
           headers: {
             'Content-Type': 'application/json',
           },
@@ -76,7 +135,7 @@ const TchProfile = () => {
       const fetchTransport = () => {
         axios({
           method: 'GET',
-          url: `${BASE_URL}/transport/getTransport/${studentDetails.routeName}`,
+          url: `${BASE_URL}/transport/getTransport/${teacherDetails.routeName}`,
           headers: {
             'Content-Type': 'application/json',
           },
@@ -94,18 +153,19 @@ const TchProfile = () => {
       fetchTransport() ;
       fetchHostelRooms()
     }
-  } , [studentDetails])
+  } , [teacherDetails])
 
-  if (!studentDetails) return <div>Loading...</div>;
+  if (!teacherDetails  || !teacherBasicDetails) return <div>Loading...</div>;
 
-  const formattedDate = new Date(studentDetails.dateOfBirth).toDateString();
+  const formattedDate = new Date(teacherDetails.dateOfBirth).toISOString().split('T')[0];
+  console.log('formattedDate',formattedDate)
 
   return (
     <div className="mb-6">
       <h1 className="text-lg md:text-2xl pt-8 font-semibold text-black">Profile</h1>
       <p className="mt-2">
         Dashboard /
-        <NavLink to="/studentDashboard"> Student </NavLink>/
+        <NavLink to="/teacherDashboard"> Teacher </NavLink>/
         <span className="text-[#ffae01] font-semibold">Profile</span>
       </p>
       
@@ -113,16 +173,16 @@ const TchProfile = () => {
           {/* Student Overview Section */}
           <div className="bg-white shadow-lg rounded-lg p-6 mb-6 flex gap-5">
           <img 
-            src={studentDetails.gender === "Male" ? maleImg : femaleImg} 
+            src={teacherDetails.gender === "Male" ? maleImg : femaleImg} 
             alt="Student" 
             className="w-24 h-24 rounded-full border-2 border-gray-300" 
           />
             <div className=" flex flex-col gap-1">
-                <StatusButton isActive={studentDetails.isActive} />
+                <StatusButton isActive={teacherDetails.isActive} />
               <h2 className="text-2xl font-bold text-gray-800">
-                {studentDetails.firstName} {studentDetails.lastName}
+                {teacherDetails.firstName} {teacherDetails.lastName}
               </h2>
-              <p className="text-sm text-gray-500">{studentDetails.admissionNumber}</p>
+              <p className="text-sm text-gray-500">{teacherDetails.admissionNumber}</p>
             </div>
           </div>
 
@@ -130,25 +190,52 @@ const TchProfile = () => {
           <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
         <h3 className="text-lg font-semibold text-gray-700 mb-4">Basic Information</h3>
         <div className="grid grid-cols-2 gap-x-4 gap-y-2 text-gray-600">
-            <p><strong>Roll No:</strong></p> 
-            <p>{studentDetails.rollNumber}</p>
+            {/* <p><strong>Roll No:</strong></p> 
+            <p>{teacherDetails.rollNumber}</p> */}
 
             <p><strong>Gender:</strong></p> 
-            <p>{studentDetails.gender}</p>
+            <p>{teacherDetails.gender}</p>
 
             <p><strong>Date of Birth:</strong></p> 
             <p>{formattedDate}</p>
 
             <p><strong>Blood Group:</strong></p> 
-            <p>{studentDetails.bloodGroup}</p>
+            <p>{teacherDetails.bloodGroup}</p>
 
             <p><strong>Religion:</strong></p> 
-            <p>{studentDetails.religion}</p>
+            <p>{teacherDetails.religion}</p>
 
             <p><strong>Caste:</strong></p> 
-            <p>{studentDetails.casteCategory}</p>
+            <p>{teacherDetails.casteCategory}</p>
+
+            <p><strong>Classes:</strong></p> 
+            <p>
+        {teacherDetails.className && teacherDetails.className.length > 0 ? (
+            teacherDetails.className.map(classId => {
+                const classDetails = filteredClasses.find(classItem => classItem.id === classId);
+                return classDetails ? `${classDetails.name} - ${classDetails.section}` : null;
+            }).join(", ")
+        ) : "No classes assigned"}
+    </p>
+            </div>
         </div>
-        </div>
+
+        <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+  <h3 className="text-lg font-semibold text-gray-700 mb-4">Profile Details</h3>
+  <div className="grid grid-cols-2 gap-x-4 gap-y-2 text-gray-600">
+    <p><strong>Father's Name:</strong></p>
+    <p>{teacherDetails.fatherName}</p>
+
+    <p><strong>Mother's Name:</strong></p>
+    <p>{teacherDetails.motherName}</p>
+
+    <p><strong>Date of Birth:</strong></p>
+    <p>{formattedDate}</p>
+
+    <p><strong>Marital Status:</strong></p>
+    <p>{teacherBasicDetails.maritalStatus}</p>
+    </div>
+    </div>
 
 
         {/* Primary Info Section */}
@@ -157,11 +244,11 @@ const TchProfile = () => {
             <ul className="space-y-2 text-gray-600">
               <li className="flex items-center space-x-3">
                 <FontAwesomeIcon icon={faPhoneAlt} />
-                <span><strong>Phone Number:</strong> {studentDetails.phone}</span>
+                <span><strong>Phone Number:</strong> {teacherDetails.phone}</span>
               </li>
               <li className="flex items-center space-x-3">
                 <FontAwesomeIcon icon={faEnvelope} />
-                <span><strong>Email:</strong> {studentDetails.email}</span>
+                <span><strong>Email:</strong> {teacherDetails.email}</span>
               </li>
             </ul>
         </div>
@@ -169,13 +256,77 @@ const TchProfile = () => {
 
 
           {/* Sibling Information */}
-          <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+          {/* <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
             <h3 className="text-lg font-semibold text-gray-700 mb-4">Sibling Information</h3>
             <ul className="space-y-2 text-gray-600">
               <li><strong>Ralph Claudia:</strong> III, B</li>
               <li><strong>Julie Scott:</strong> V, A</li>
             </ul>
-          </div>
+          </div> */}
+
+            {/* Qualification Section */}
+            <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+    <h3 className="text-lg font-semibold text-gray-700 mb-4">Qualification</h3>
+    <table className="w-full border-collapse">
+        <thead>
+            <tr>
+                <th className="text-left px-4 py-2">Course</th>
+                <th className="text-left px-4 py-2">Institute</th>
+                <th className="text-left px-4 py-2">Passout Year</th>
+                <th className="text-left px-4 py-2">CGPA</th>
+            </tr>
+        </thead>
+        <tbody>
+        {teacherBasicDetails?.qualificationList && teacherBasicDetails.qualificationList.length > 0 ? (
+                teacherBasicDetails.qualificationList.map((qual) => (
+                    <tr key={qual.id} className="border-t">
+                        <td className="px-4 py-2">{qual.course}</td>
+                        <td className="px-4 py-2">{qual.institute}</td>
+                        <td className="px-4 py-2">{qual.passoutYear}</td>
+                        <td className="px-4 py-2">{qual.cgpa}</td>
+                    </tr>
+                ))
+            ) : (
+                <tr>
+                    <td colSpan="4" className="text-center py-4">No qualifications available</td>
+                </tr>
+            )}
+        </tbody>
+    </table>
+</div>
+
+
+            {/* Work Experience Section */}
+            <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+            <h3 className="text-lg font-semibold text-gray-700 mb-4">Work Experience</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="text-left px-4 py-2">From Year</th>
+                            <th className="text-left px-4 py-2">To Year</th>
+                            <th className="text-left px-4 py-2">Institute</th>
+                            <th className="text-left px-4 py-2">Designation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {teacherBasicDetails?.workExperience && teacherBasicDetails.workExperience.length > 0 ? (
+                            teacherBasicDetails.workExperience.map((work) => (
+                                <tr key={work.id}>
+                                    <td className="px-4 py-2">{work.fromYear}</td>
+                                    <td className="px-4 py-2">{work.toYear}</td>
+                                    <td className="px-4 py-2">{work.insitutue}</td>
+                                    <td className="px-4 py-2">{work.designation}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="4">No work experience available</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+
 
           {/* Hostel and Transport Section */}
           <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
@@ -216,7 +367,7 @@ const TchProfile = () => {
                 <p><strong>Driver Phone Number:</strong></p>
                 <p>{transport.phone}</p>
                 <p><strong>Pickup Point:</strong></p>
-                <p>{studentDetails.pickupPoint}</p>
+                <p>{teacherDetails.pickupPoint}</p>
               </div>
             )}
           </div>
@@ -228,10 +379,10 @@ const TchProfile = () => {
             <h3 className="text-lg font-semibold text-gray-700mb-4">Parents Information</h3>
             <div className="space-y-2 text-gray-600">
               <div>
-                <p><strong>Father Name:</strong> {studentDetails.fatherName}</p>
+                <p><strong>Father Name:</strong> {teacherDetails.fatherName}</p>
               </div>
               <div>
-                <p><strong>Mother Name:</strong> {studentDetails.motherName}</p>
+                <p><strong>Mother Name:</strong> {teacherDetails.motherName}</p>
               </div>
             </div>
           </div>
@@ -240,7 +391,7 @@ const TchProfile = () => {
         <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
         <h3 className="text-lg font-semibold text-gray-700 mb-4">Documents</h3>
         <div className="space-y-4">
-            {/* {studentDetails.documents.map((doc, index) => ( */}
+            {/* {teacherDetails.documents.map((doc, index) => ( */}
             <div key ="" className="flex items-center justify-between bg-gray-100 p-4 rounded-lg shadow">
                 <div className="flex items-center space-x-3">
                 <FontAwesomeIcon icon={faFilePdf} />
@@ -258,19 +409,19 @@ const TchProfile = () => {
         <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
         <h3 className="text-lg font-semibold text-gray-700 mb-4">Previous School Details</h3>
         <ul className=" text-gray-600 flex justify-between ">
-            <li className='flex flex-col'><strong>School Name</strong> {studentDetails.previousSchoolName}</li>
-            <li className='flex flex-col'><strong>Address</strong> {studentDetails.preSchoolAddress}</li>
-            <li className='flex flex-col'><strong>Leaving Year</strong> {studentDetails.preSchoolLeavingSession}</li>
+            <li className='flex flex-col'><strong>School Name</strong> {teacherBasicDetails?.previousSchool}</li>
+            <li className='flex flex-col'><strong>Address</strong> {teacherDetails.preSchoolAddress}</li>
+            <li className='flex flex-col'><strong>Leaving Year</strong> {teacherDetails.preSchoolLeavingSession}</li>
         </ul>
         </div>
 
         <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
         <h3 className="text-lg font-semibold text-gray-700 mb-4">Address</h3>
         <ul className="space-y-2 text-gray-600">
-            <li><strong>House No:</strong> {studentDetails.houseNumber}</li>
-            <li><strong>Street:</strong> {studentDetails.street}</li>
-            <li><strong>City:</strong> {studentDetails.city}</li>
-            <li><strong>Pincode:</strong> {studentDetails.pinCode}</li>
+            <li><strong>House No:</strong> {teacherDetails.houseNumber}</li>
+            <li><strong>Street:</strong> {teacherDetails.street}</li>
+            <li><strong>City:</strong> {teacherDetails.city}</li>
+            <li><strong>Pincode:</strong> {teacherDetails.pinCode}</li>
         </ul>
         </div>
 
@@ -281,9 +432,11 @@ const TchProfile = () => {
           <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
             <h3 className="text-lg font-semibold text-gray-700 mb-4">Bank Details</h3>
             <ul className="space-y-2 text-gray-600">
-              <li><strong>Bank Name:</strong> Bank of America</li>
-              <li><strong>Branch:</strong> Cincinnati</li>
-              <li><strong>IFSC:</strong> BOA83209832</li>
+              <li><strong>Bank Name:</strong>{teacherBasicDetails?.bankName}</li>
+              <li><strong>Branch:</strong> {teacherBasicDetails?.branchName}</li>
+              <li><strong>IFSC:</strong> {teacherBasicDetails?.ifsc}</li>
+              <li><strong>Account Name:</strong> {teacherBasicDetails?.accountName}</li>
+              <li><strong>PAN</strong> {teacherBasicDetails?.pan}</li>
             </ul>
           </div>
 
