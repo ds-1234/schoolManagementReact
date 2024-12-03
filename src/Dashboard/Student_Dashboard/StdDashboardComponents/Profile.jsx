6f649ead9d30011cd0027231af536d032b842ad4
uddeshya tyagi <uddeshyatyagi123@gmail.com>
2024-12-03 18:47:39 +0530

    Student profile

--- src/Dashboard/Student_Dashboard/StdDashboardComponents/Profile.jsx
+++ src/Dashboard/Student_Dashboard/StdDashboardComponents/Profile.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../../Reusable_components/Button";
 import { useNavigate } from "react-router-dom";
+import BASE_URL from '../../../conf/conf';
+import axios from "axios";
+
 
 function Profile({ name, className, rollNo }) {
   const navigate = useNavigate();
+  const [classes,setClasses] = useState([])
+  // Fetch subject data from API
+  useEffect(() => {
+    const fetchClasses = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/class/getClassList`);
+        if (response.data.success) {
+          setClasses(response.data.data);
+        }
+        console.log(response.data.data,'refawef')
+      } catch (error) {
+        console.error('Error fetching Classes:', error);
+      }
+    };
+
+    fetchClasses();
+  }, []);
+
+  const getClassNameById = (id) => {
+    const cls = classes.find((cls) => cls.id == id);
+    return cls ? cls.name : `Class ${id}`;
+  };
+  console.log(classes,'classes')
 
   return (
     <div className="w-[400px] bg-blue-700 rounded-lg p-6 text-white shadow-lg">
@@ -13,7 +39,7 @@ function Profile({ name, className, rollNo }) {
         <div className="ml-6">
           <h2 className="text-2xl font-semibold">{name}</h2>
           <p className="text-base">
-            Class: {className} | Roll No: {rollNo}
+            Class: {getClassNameById(className)} | Roll No: {rollNo}
           </p>
         </div>
       </div>
