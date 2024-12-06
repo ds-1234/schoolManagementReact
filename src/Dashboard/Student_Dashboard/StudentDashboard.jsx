67db070ae8fb14b8a25979dedc0b2636b97f67e6
YukiP7 <gargyukti112@gmail.com>
2024-12-06 21:20:59 +0530

    add responsive in student & teacher role

--- src/Dashboard/Student_Dashboard/StudentDashboard.jsx
+++ src/Dashboard/Student_Dashboard/StudentDashboard.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React , {useState} from 'react';
 import {Outlet } from 'react-router-dom';
 import Layout from '../../Reusable_components/Layout';
 import HeaderBar from '../../Reusable_components/HeaderBar';
 import StdSidebar from '../../Reusable_components/Sidebars/StdSidebar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function StudentDashboard() {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
     <Layout>
-    <div className='flex gap-10'>
-      {/* Sidebar */}
-      <div className=" w-1/3 text-white flex flex-col sm:w-1/5 ">
-       <StdSidebar/>
-     </div>
+    <div className="relative flex gap-10 2xl:gap-5 2xl:m-0 overflow-hidden">
+        {/* Sidebar */}
+        <div
+          className={`fixed top-0 left-0 z-40 h-full w-3/4  transform ${
+            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } transition-transform duration-300 ease-in-out xl:static xl:transform-none xl:w-1/6`}
+        >
+          <StdSidebar />
+        </div>
 
-     {/* Main content */}
-     <div className='w-full mr-10'>
-      <HeaderBar/>
-       <Outlet />
-     </div>
-    </div>
+        {/* Toggle Button */}
+        <button
+          className={`xl:hidden absolute top-0 z-50 p-2 text-gray-950 ${isSidebarOpen ? "left-40 text-white" : 'top-6 left-4'}`}
+          onClick={toggleSidebar}
+        >
+          <FontAwesomeIcon icon={isSidebarOpen ? faTimes : faBars}  />
+        </button>
+
+        {/* Main Content */}
+        <div className="w-full xl:mr-10 p-4 xl:p-0">
+          <HeaderBar />
+          <Outlet />
+        </div>
+
+        {/* Overlay for small screens when sidebar is open */}
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-30 xl:hidden"
+            onClick={toggleSidebar}
+          ></div>
+        )}
+      </div>
    </Layout>
   )
 }
--- src/Dashboard/Teacher_Dashboard/TeacherDashboard.jsx
+++ src/Dashboard/Teacher_Dashboard/TeacherDashboard.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React , { useState} from 'react';
 import {Outlet } from 'react-router-dom';
 import Layout from '../../Reusable_components/Layout';
 import TchSidebar from '../../Reusable_components/Sidebars/TchSidebar';
 import HeaderBar from '../../Reusable_components/HeaderBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function TeacherDashboard() {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
     <Layout>
-     <div className='flex flex-col md:flex-row gap-5 md:m-0 overflow-hidden'>
-       {/* Sidebar */}
-       <div className=" w-full md:w-1/6 text-white flex flex-col h-full ">
-        <TchSidebar/>
-      </div>
+     <div className="relative flex gap-5 2xl:gap-5 2xl:m-0 overflow-hidden">
+        {/* Sidebar */}
+        <div
+          className={`fixed top-0 left-0 z-40 h-full w-3/4  transform ${
+            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } transition-transform duration-300 ease-in-out xl:static xl:transform-none xl:w-1/6`}
+        >
+          <TchSidebar />
+        </div>
 
-      {/* Main content */} 
-      <div className="w-full md:mr-10 ">
-        <HeaderBar/>
-        <Outlet />
+        {/* Toggle Button */}
+        <button
+          className={`xl:hidden absolute top-0 z-50 p-2 text-gray-950 ${isSidebarOpen ? "left-40 text-white" : 'top-6 left-4'}`}
+          onClick={toggleSidebar}
+        >
+          <FontAwesomeIcon icon={isSidebarOpen ? faTimes : faBars}  />
+        </button>
+
+        {/* Main Content */}
+        <div className="w-full xl:mr-10 p-4 xl:p-0">
+          <HeaderBar />
+          <Outlet />
+        </div>
+
+        {/* Overlay for small screens when sidebar is open */}
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 z-30 xl:hidden"
+            onClick={toggleSidebar}
+          ></div>
+        )}
       </div>
-     </div>
     </Layout>
   )
 }
